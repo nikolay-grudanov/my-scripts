308d4ddc6551d5dd3b13af955a87ed311a4f22bb
Груданов Николай <grudanov.n.a@gmail.com>
2022-10-23 23:25:38 +0300

    Настроил гитигнор и добавил скрипт для для uuid

---
+++ edu/hellow.py
@@ -0,0 +1,3 @@
+#!/Users/nikolajgrudanov/miniconda3/envs/someScripts/bin/python3
+print("Hellow World!")
+
---
+++ for-my-pc/createUUID4.py
@@ -0,0 +1,5 @@
+#!/Users/nikolajgrudanov/miniconda3/envs/someScripts/bin/python3
+import uuid
+
+id = uuid.uuid4()
+print(id)
